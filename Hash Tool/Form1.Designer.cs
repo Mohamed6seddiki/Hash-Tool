950470dc96144cd6334d0af4e9f15ca4d6f51618
Mohamed6seddiki <seddikimoh84@gmail.com>
2025-08-24 13:25:57 +0100

    adding all files

---
+++ Hash Tool/Form1.Designer.cs
@@ -0,0 +1,422 @@
+﻿namespace Hash_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.MD2 = new System.Windows.Forms.CheckBox();
+            this.MD5 = new System.Windows.Forms.CheckBox();
+            this.SHA2384 = new System.Windows.Forms.CheckBox();
+            this.SHA2256 = new System.Windows.Forms.CheckBox();
+            this.SHA2224 = new System.Windows.Forms.CheckBox();
+            this.SHA1 = new System.Windows.Forms.CheckBox();
+            this.RIPEMD160 = new System.Windows.Forms.CheckBox();
+            this.SHA2512 = new System.Windows.Forms.CheckBox();
+            this.SHA3224 = new System.Windows.Forms.CheckBox();
+            this.SHA3384 = new System.Windows.Forms.CheckBox();
+            this.SHA3512 = new System.Windows.Forms.CheckBox();
+            this.WHIRLPOOL = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SHA3256 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Tai Le", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(375, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(273, 49);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "enter your text";
+            // 
+            // MD2
+            // 
+            this.MD2.AutoSize = true;
+            this.MD2.Location = new System.Drawing.Point(99, 110);
+            this.MD2.Name = "MD2";
+            this.MD2.Size = new System.Drawing.Size(57, 20);
+            this.MD2.TabIndex = 2;
+            this.MD2.Text = "MD2";
+            this.MD2.UseVisualStyleBackColor = true;
+            // 
+            // MD5
+            // 
+            this.MD5.AutoSize = true;
+            this.MD5.Location = new System.Drawing.Point(99, 152);
+            this.MD5.Name = "MD5";
+            this.MD5.Size = new System.Drawing.Size(57, 20);
+            this.MD5.TabIndex = 3;
+            this.MD5.Text = "MD5";
+            this.MD5.UseVisualStyleBackColor = true;
+            // 
+            // SHA2384
+            // 
+            this.SHA2384.AutoSize = true;
+            this.SHA2384.Location = new System.Drawing.Point(99, 362);
+            this.SHA2384.Name = "SHA2384";
+            this.SHA2384.Size = new System.Drawing.Size(91, 20);
+            this.SHA2384.TabIndex = 4;
+            this.SHA2384.Text = "SHA.2 384";
+            this.SHA2384.UseVisualStyleBackColor = true;
+            // 
+            // SHA2256
+            // 
+            this.SHA2256.AutoSize = true;
+            this.SHA2256.Location = new System.Drawing.Point(99, 320);
+            this.SHA2256.Name = "SHA2256";
+            this.SHA2256.Size = new System.Drawing.Size(91, 20);
+            this.SHA2256.TabIndex = 5;
+            this.SHA2256.Text = "SHA.2 256";
+            this.SHA2256.UseVisualStyleBackColor = true;
+            // 
+            // SHA2224
+            // 
+            this.SHA2224.AutoSize = true;
+            this.SHA2224.Location = new System.Drawing.Point(99, 278);
+            this.SHA2224.Name = "SHA2224";
+            this.SHA2224.Size = new System.Drawing.Size(91, 20);
+            this.SHA2224.TabIndex = 6;
+            this.SHA2224.Text = "SHA.2 224";
+            this.SHA2224.UseVisualStyleBackColor = true;
+            // 
+            // SHA1
+            // 
+            this.SHA1.AutoSize = true;
+            this.SHA1.Location = new System.Drawing.Point(99, 236);
+            this.SHA1.Name = "SHA1";
+            this.SHA1.Size = new System.Drawing.Size(67, 20);
+            this.SHA1.TabIndex = 7;
+            this.SHA1.Text = "SHA.1";
+            this.SHA1.UseVisualStyleBackColor = true;
+            // 
+            // RIPEMD160
+            // 
+            this.RIPEMD160.AutoSize = true;
+            this.RIPEMD160.Location = new System.Drawing.Point(99, 194);
+            this.RIPEMD160.Name = "RIPEMD160";
+            this.RIPEMD160.Size = new System.Drawing.Size(106, 20);
+            this.RIPEMD160.TabIndex = 8;
+            this.RIPEMD160.Text = "RIPEMD-160";
+            this.RIPEMD160.UseVisualStyleBackColor = true;
+            // 
+            // SHA2512
+            // 
+            this.SHA2512.AutoSize = true;
+            this.SHA2512.Location = new System.Drawing.Point(99, 404);
+            this.SHA2512.Name = "SHA2512";
+            this.SHA2512.Size = new System.Drawing.Size(91, 20);
+            this.SHA2512.TabIndex = 9;
+            this.SHA2512.Text = "SHA.2 512";
+            this.SHA2512.UseVisualStyleBackColor = true;
+            // 
+            // SHA3224
+            // 
+            this.SHA3224.AutoSize = true;
+            this.SHA3224.Location = new System.Drawing.Point(99, 446);
+            this.SHA3224.Name = "SHA3224";
+            this.SHA3224.Size = new System.Drawing.Size(91, 20);
+            this.SHA3224.TabIndex = 10;
+            this.SHA3224.Text = "SHA.3 224";
+            this.SHA3224.UseVisualStyleBackColor = true;
+            // 
+            // SHA3384
+            // 
+            this.SHA3384.AutoSize = true;
+            this.SHA3384.Location = new System.Drawing.Point(99, 530);
+            this.SHA3384.Name = "SHA3384";
+            this.SHA3384.Size = new System.Drawing.Size(91, 20);
+            this.SHA3384.TabIndex = 11;
+            this.SHA3384.Text = "SHA.3 384";
+            this.SHA3384.UseVisualStyleBackColor = true;
+            // 
+            // SHA3512
+            // 
+            this.SHA3512.AutoSize = true;
+            this.SHA3512.Location = new System.Drawing.Point(99, 572);
+            this.SHA3512.Name = "SHA3512";
+            this.SHA3512.Size = new System.Drawing.Size(91, 20);
+            this.SHA3512.TabIndex = 12;
+            this.SHA3512.Text = "SHA.3 512";
+            this.SHA3512.UseVisualStyleBackColor = true;
+            // 
+            // WHIRLPOOL
+            // 
+            this.WHIRLPOOL.AutoSize = true;
+            this.WHIRLPOOL.Location = new System.Drawing.Point(99, 614);
+            this.WHIRLPOOL.Name = "WHIRLPOOL";
+            this.WHIRLPOOL.Size = new System.Drawing.Size(108, 20);
+            this.WHIRLPOOL.TabIndex = 13;
+            this.WHIRLPOOL.Text = "WHIRLPOOL";
+            this.WHIRLPOOL.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(90, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 50);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Plain text";
+            // 
+            // SHA3256
+            // 
+            this.SHA3256.AutoSize = true;
+            this.SHA3256.Location = new System.Drawing.Point(99, 488);
+            this.SHA3256.Name = "SHA3256";
+            this.SHA3256.Size = new System.Drawing.Size(91, 20);
+            this.SHA3256.TabIndex = 15;
+            this.SHA3256.Text = "SHA.3 256";
+            this.SHA3256.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(88, 678);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(328, 58);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "HASH";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(452, 678);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(185, 58);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Destroy All";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(365, 103);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(282, 22);
+            this.textBox2.TabIndex = 18;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(365, 481);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(282, 22);
+            this.textBox3.TabIndex = 19;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(365, 439);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(282, 22);
+            this.textBox4.TabIndex = 20;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(365, 397);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(282, 22);
+            this.textBox5.TabIndex = 21;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(365, 355);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(282, 22);
+            this.textBox6.TabIndex = 22;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(365, 313);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(282, 22);
+            this.textBox7.TabIndex = 23;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(365, 229);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.ReadOnly = true;
+            this.textBox10.Size = new System.Drawing.Size(282, 22);
+            this.textBox10.TabIndex = 26;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(365, 607);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.ReadOnly = true;
+            this.textBox11.Size = new System.Drawing.Size(282, 22);
+            this.textBox11.TabIndex = 27;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(365, 565);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.ReadOnly = true;
+            this.textBox12.Size = new System.Drawing.Size(282, 22);
+            this.textBox12.TabIndex = 28;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(365, 523);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.ReadOnly = true;
+            this.textBox13.Size = new System.Drawing.Size(282, 22);
+            this.textBox13.TabIndex = 29;
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(365, 271);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.ReadOnly = true;
+            this.textBox14.Size = new System.Drawing.Size(282, 22);
+            this.textBox14.TabIndex = 30;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(365, 187);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.ReadOnly = true;
+            this.textBox9.Size = new System.Drawing.Size(282, 22);
+            this.textBox9.TabIndex = 25;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(365, 145);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(282, 22);
+            this.textBox8.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(676, 763);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SHA3256);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.WHIRLPOOL);
+            this.Controls.Add(this.SHA3512);
+            this.Controls.Add(this.SHA3384);
+            this.Controls.Add(this.SHA3224);
+            this.Controls.Add(this.SHA2512);
+            this.Controls.Add(this.RIPEMD160);
+            this.Controls.Add(this.SHA1);
+            this.Controls.Add(this.SHA2224);
+            this.Controls.Add(this.SHA2256);
+            this.Controls.Add(this.SHA2384);
+            this.Controls.Add(this.MD5);
+            this.Controls.Add(this.MD2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Hash Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox MD2;
+        private System.Windows.Forms.CheckBox MD5;
+        private System.Windows.Forms.CheckBox SHA2384;
+        private System.Windows.Forms.CheckBox SHA2256;
+        private System.Windows.Forms.CheckBox SHA2224;
+        private System.Windows.Forms.CheckBox SHA1;
+        private System.Windows.Forms.CheckBox RIPEMD160;
+        private System.Windows.Forms.CheckBox SHA2512;
+        private System.Windows.Forms.CheckBox SHA3224;
+        private System.Windows.Forms.CheckBox SHA3384;
+        private System.Windows.Forms.CheckBox SHA3512;
+        private System.Windows.Forms.CheckBox WHIRLPOOL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox SHA3256;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+    }
+}
+
---
+++ Hash Tool/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Digests;
+
+namespace Hash_Tool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string ComputeHash(string input, Func<HashAlgorithm> algorithmFactory)
+        {
+            using (HashAlgorithm algorithm = algorithmFactory())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = algorithm.ComputeHash(inputBytes);
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in hashBytes)
+                    sb.Append(b.ToString("x2"));
+                return sb.ToString();
+            }
+        }
+
+        private string ComputeBouncyHash(string input, IDigest digest)
+        {
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            digest.BlockUpdate(inputBytes, 0, inputBytes.Length);
+            byte[] result = new byte[digest.GetDigestSize()];
+            digest.DoFinal(result, 0);
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in result)
+                sb.Append(b.ToString("x2"));
+            return sb.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+
+            // MD2
+            if (MD2.Checked)
+                textBox2.Text = ComputeBouncyHash(text, new MD2Digest());
+
+            // MD5 - Using fully qualified name
+            if (MD5.Checked)
+                textBox8.Text = ComputeHash(text, () => System.Security.Cryptography.MD5.Create());
+
+            // RIPEMD160 - Using fully qualified name
+            if (RIPEMD160.Checked)
+                textBox9.Text = ComputeHash(text, () => System.Security.Cryptography.RIPEMD160.Create());
+
+            // SHA1 - Using fully qualified name
+            if (SHA1.Checked)
+                textBox10.Text = ComputeHash(text, () => System.Security.Cryptography.SHA1.Create());
+
+            // SHA-224 (BouncyCastle)
+            if (SHA2224.Checked)
+                textBox14.Text = ComputeBouncyHash(text, new Sha224Digest());
+
+            // SHA-256
+            if (SHA2256.Checked)
+                textBox7.Text = ComputeHash(text, () => SHA256.Create());
+
+            // SHA-384
+            if (SHA2384.Checked)
+                textBox6.Text = ComputeHash(text, () => SHA384.Create());
+
+            // SHA-512
+            if (SHA2512.Checked)
+                textBox5.Text = ComputeHash(text, () => SHA512.Create());
+
+            // SHA3-224
+            if (SHA3224.Checked)
+                textBox4.Text = ComputeBouncyHash(text, new Sha3Digest(224));
+
+            // SHA3-256
+            if (SHA3256.Checked)
+                textBox3.Text = ComputeBouncyHash(text, new Sha3Digest(256));
+
+            // SHA3-384
+            if (SHA3384.Checked)
+                textBox13.Text = ComputeBouncyHash(text, new Sha3Digest(384));
+
+            // SHA3-512
+            if (SHA3512.Checked)
+                textBox12.Text = ComputeBouncyHash(text, new Sha3Digest(512));
+
+            // Whirlpool
+            if (WHIRLPOOL.Checked)
+                textBox11.Text = ComputeBouncyHash(text, new WhirlpoolDigest());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text= textBox3.Text = textBox4.Text = textBox5.Text = textBox6.Text = textBox7.Text = textBox8.Text = textBox9.Text = textBox10.Text = textBox11.Text = textBox12.Text = textBox13.Text = textBox14.Text = "";
+        }
+    }
+}
